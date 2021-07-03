f53e2e46955fc52b8275ddd0fd4005c61db5843f
dehbaiyor <dehbaiyor@gmail.com>
2021-07-03 22:11:48 +0200

    added codes till training

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -54,7 +54,8 @@ def go(args):
     ######################################
     # Use run.use_artifact(...).file() to get the train and validation artifact (args.trainval_artifact)
     # and save the returned path in train_local_pat
-    trainval_local_path = # YOUR CODE HERE
+    trainval_local_path = run.use_artifact(args.trainval_artifact).file()
+    
     ######################################
 
     X = pd.read_csv(trainval_local_path)
@@ -75,9 +76,9 @@ def go(args):
 
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)
     ######################################
-
+    
     # Compute r2 and MAE
     logger.info("Scoring")
     r_squared = sk_pipe.score(X_val, y_val)
@@ -87,9 +88,9 @@ def go(args):
 
     logger.info(f"Score: {r_squared}")
     logger.info(f"MAE: {mae}")
-
+    
     logger.info("Exporting model")
-
+    
     # Save model package in the MLFlow sklearn format
     if os.path.exists("random_forest_dir"):
         shutil.rmtree("random_forest_dir")
@@ -97,8 +98,13 @@ def go(args):
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
-    # YOUR CODE HERE
     ######################################
+    mlflow.sklearn.save_model(
+            sk_pipe,
+            random_forest_dir,
+            serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
+            input_example=X_val.iloc[:2],
+        )
 
     ######################################
     # Upload the model we just exported to W&B
@@ -106,8 +112,16 @@ def go(args):
     # type, provide a description and add rf_config as metadata. Then, use the .add_dir method of the artifact instance
     # you just created to add the "random_forest_dir" directory to the artifact, and finally use
     # run.log_artifact to log the artifact to the run
-    # YOUR CODE HERE
     ######################################
+    artifact = wandb.Artifact(
+            args.output_artifact,
+            type="model_export",
+            metadata=rf_config
+            description="Random Forest pipeline export",
+        )
+    
+    artifact.add_dir(export_path)
+    run.log_artifact(artifact)
 
     # Plot feature importance
     fig_feat_imp = plot_feature_importance(sk_pipe, processed_features)
@@ -115,8 +129,8 @@ def go(args):
     ######################################
     # Here we save r_squared under the "r2" key
     run.summary['r2'] = r_squared
+    run.summary['mae'] = mae
     # Now log the variable "mae" under the key "mae".
-    # YOUR CODE HERE
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -158,7 +172,9 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # Build a pipeline with two steps:
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
-    non_ordinal_categorical_preproc = # YOUR CODE HERE
+    non_ordinal_categorical_preproc = make_pipeline(
+        SimpleImputer(strategy='most_frequent'),
+        OneHotEncoder())
     ######################################
 
     # Let's impute the numerical columns to make sure we can handle missing values
@@ -217,7 +233,11 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # ColumnTransformer instance that we saved in the `preprocessor` variable, and a step called "random_forest"
     # with the random forest instance that we just saved in the `random_forest` variable.
     # HINT: Use the explicit Pipeline constructor so you can assign the names to the steps, do not use make_pipeline
-    sk_pipe = # YOUR CODE HERE
+    sk_pipe = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("random_forest", RandomForestRegressor(**model_config["random_forest"])),
+        ]
 
     return sk_pipe, processed_features
 
