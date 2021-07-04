cb1c300363f06faac02e819b6206457c4e64466f
dehbaiyor <dehbaiyor@gmail.com>
2021-07-04 09:08:43 +0200

    set proper boundaries fixed

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,10 @@ def go(args):
     logger.info("Converting last review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
     
+    # Drop rows not in proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     #Logging to Weights and Biases
     logger.info("Logging clean sample to Weights and Biases")
     df.to_csv("clean_sample.csv", index=False)
